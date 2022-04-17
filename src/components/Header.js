ac94d7a569c2107e7492bd853327c31bca4f7a1c
sieun95 <92782143+sieun95@users.noreply.github.com>
2022-04-17 20:41:32 +0900

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { Navbar } from 'react-bootstrap'
 import { Nav } from 'react-bootstrap'
-import cart from "../images/icons/cart.png"
+// import cart from "../images/icons/cart.png"
 import logo from "../images/icons/logo.png"
+import cart from "../images/icons/cart.png"
+import "./Header.css"
 
 
 function Header() {
@@ -12,14 +14,14 @@ function Header() {
             <Navbar className='w-100' collapseOnSelect expand='lg' variant='dark'>
                 <Navbar.Toggle aria-controls='responsive-navbar-nav'/>
                 <Navbar.Brand href='/'>
-                    <img src='{logo}' alt=''/>
+                    <img src={logo} alt=''/>
                 </Navbar.Brand>
                 <Navbar.Collapse id='reponsive-navbar-nav'>
                     <Nav className='nav-justified w-100 nav-fill'>
                         <Nav.Link href='/'>iphone</Nav.Link>
                         <Nav.Link href='/Login'>Login</Nav.Link>
                         <Nav.Link href='/user/cart'>
-                            <img src='{cart}' alt='cart'/>
+                            <img src={cart} alt=''/>
                         </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
